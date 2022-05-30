eb493782dcb59408c24f21f7a440a04c730991c3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-05-30 14:08:36 -0700

    Added doxygen file and GHA for doc building.

--- include/raticate/raticate.hpp
+++ include/raticate/raticate.hpp
@@ -7,8 +7,30 @@
 #include "CSparseMatrix.hpp"
 #include "utils.hpp"
 
+/**
+ * @file raticate.hpp
+ *
+ * @brief Parse `Rcpp::RObject`s into **tatami** matrices.
+ */
+
 namespace raticate {
 
+/**
+ * Parse `Rcpp::RObject`s into **tatami** matrices.
+ * Supported matrix types are:
+ *
+ * - ordinary logical, numeric or integer matrices.
+ * - `dgCMatrix` or `lgCMatrix` objects from the **Matrix** package.
+ * - `SparseArraySeed` objects from the **DelayedArray** package.
+ * 
+ * @tparam Data Numeric data type for the **tatami** interface.
+ * @tparam Index Integer index type for the **tatami** interface.
+ * 
+ * @param x An R object representing a supported matrix type.
+ *
+ * @return A `Parsed` object containing a pointer to a parsed `tatami::Matrix`.
+ * If parsing was not successful, this pointer will be a `nullptr`.
+ */
 template<typename Data = double, typename Index = int>
 Parsed<Data, Index> parse(Rcpp::RObject x) {
     Parsed<Data, Index> output;
