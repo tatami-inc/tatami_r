c4ae067393031a928fc746b03fe37112c1d5057c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-05-30 21:33:22 +0000

    Deploying to gh-pages from @ LTLA/raticate@f054f6af3d59c3a1af7d2c8c5fbb824c44cee8ce 🚀

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['contents_0',['contents',['../structraticate_1_1Parsed.html#a2ff46b710c05d7bf912e33cb8bb7c510',1,'raticate::Parsed']]],
-  ['convert_20r_20objects_20to_20_3ctt_3etatami_3c_2ftt_3e_20objects_1',['Convert R objects to &lt;tt&gt;tatami&lt;/tt&gt; objects',['../index.html',1,'']]]
+  ['convert_20r_20objects_20to_20_3ctt_3etatami_3c_2ftt_3e_20matrices_1',['Convert R objects to &lt;tt&gt;tatami&lt;/tt&gt; matrices',['../index.html',1,'']]]
 ];
--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['convert_20r_20objects_20to_20_3ctt_3etatami_3c_2ftt_3e_20objects_0',['Convert R objects to &lt;tt&gt;tatami&lt;/tt&gt; objects',['../index.html',1,'']]]
+  ['convert_20r_20objects_20to_20_3ctt_3etatami_3c_2ftt_3e_20matrices_0',['Convert R objects to &lt;tt&gt;tatami&lt;/tt&gt; matrices',['../index.html',1,'']]]
 ];
